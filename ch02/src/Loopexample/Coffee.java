34bd7a08d2e7da7d5d85d41e321d9613830e96fa
jjungyoon06 <jjungyoon06@gmail.com>
2022-06-23 14:22:05 +0900

    ch02 src

---
+++ ch02/src/Loopexample/Coffee.java
@@ -0,0 +1,43 @@
+package Loopexample;
+
+import java.util.Scanner;
+
+public class Coffee {
+
+	public static void main(String[] args) {
+		// 커피 자판기 프로그램
+			Scanner sc = new Scanner(System.in);
+			int coffee = 5; //커피의 총 수량
+			while(true) {
+				System.out.println("돈을 넣어주세요");
+				int money = sc.nextInt();
+				if(money == 500) {
+					System.out.println("커피가 나옵니다.");
+					coffee -= 1; //커피 개수 1감소
+				}
+				else if(money > 500) {
+					System.out.println("거스름돈" + (money -500) + "원을 돌려주고 커피가 나옵니다.");
+					coffee -= 1;
+				}
+				else {
+					System.out.println("돈은 돌려주고 커피는 나오지 않습니다.");
+					
+				}
+				System.out.println("남은 커피의 양은 " + coffee + "개입니다. ");
+				
+				//커피의 개수가 0이면 "판매 중지"
+				if(coffee == 0) {
+					System.out.println("커피가 다 떨어졌습니다. 판매를 중지합니다");
+					break;
+				}
+				
+				sc.close();
+			}
+				
+				
+				
+			}
+
+	}
+
+
---
+++ ch02/src/Switchcase/Case.java
@@ -0,0 +1,27 @@
+package Switchcase;
+
+public class Case {
+
+	public static void main(String[] args) {
+		// case문 동시에 사용하기
+		int month = 6;
+		int day = 0;
+		
+		switch(month) {
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			day = 31;
+			break;
+		case 4: case 6: case 9: case 11:
+			day = 30;
+			break;
+		case 2:
+			day = 28;
+			break;
+		default:
+			System.out.println("지원되지 않는 기능입니다.");
+			return; //즉시 종료
+		}
+		System.out.println(month + "월은" + day + "일까지 있습니다.");
+	}
+
+}
