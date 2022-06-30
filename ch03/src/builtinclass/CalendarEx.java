9a40525e3af9442fd14c8fe2e475b19473e12a62
jjungyoon06 <jjungyoon06@gmail.com>
2022-06-30 12:21:07 +0900

    0630

--- ch03/src/builtinclass/CalendarEx.java
+++ ch03/src/builtinclass/CalendarEx.java
@@ -17,12 +17,17 @@ public class CalendarEx {
 		int min = cal.get(Calendar.MINUTE);
 		int sec = cal.get(Calendar.SECOND);
 		
-		//요일
+		//요일 - //1-일, 2-월, 3-화... 6-토
 		int day = cal.get(Calendar.DAY_OF_WEEK);
+		
+		//현재 시간 시간 측정 - 1970.1.1 자정이후 ms 기준
+		long time = cal.getTimeInMillis();
 		
 		System.out.println(year + "년" + month + "월" + date + "일");
 		System.out.println(hour + "시" + min + "분" + sec + "초");
 		System.out.println(day); //일(1) 월(2) 화(3) ...
+		System.out.println(time + "ms");
+		System.out.println(time/1000 + "초");
 		
 		
 		
