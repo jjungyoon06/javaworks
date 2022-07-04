0558f281761423e37fb3a9dc4d1eb5a26433aa81
jjungyoon06 <jjungyoon06@gmail.com>
2022-07-04 18:21:56 +0900

    0704

---
+++ exercise/src/chap06/Test6_5.java
@@ -0,0 +1,8 @@
+package chap06;
+
+public class Test6_5 {
+	//1 0 X 0 0
+	//2 2
+	//3
+		
+}
---
+++ exercise/src/chap07/Test7_1.java
@@ -0,0 +1,13 @@
+package chap07;
+
+public class Test7_1 {
+	public static void main(String[] args) {
+		//1 X 0 0 0
+		//2 0 X 0 0 
+		//3 X X 0 X
+		//4 super(name) - 7행 에러
+		
+		Child child = new Child();
+		
+	}
+}
