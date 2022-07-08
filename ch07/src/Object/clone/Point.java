c3461674cb0ec8cdd0d75f2595839fc524978d45
jjungyoon06 <jjungyoon06@gmail.com>
2022-07-08 18:19:11 +0900

    0708

---
+++ ch07/src/Object/clone/Point.java
@@ -0,0 +1,20 @@
+package Object.clone;
+
+public class Point {
+	
+	int x, y;
+	
+	Point(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "x=" + x +", y=" + y ;
+	}
+
+	
+	
+	
+}
